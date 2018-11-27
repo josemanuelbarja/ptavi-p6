e59b13e1e1dc2371c7412e85c80f386de68d27d1
José Manuel Barja Vázquez <josemanuelbarja@MacBook-Pro-de-Jose-5.local>
2018-11-27 16:23:58 +0100

    Terminado hasta antes de captura

--- client.py
+++ client.py
@@ -5,26 +5,30 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
-# Cliente UDP simple.
+try:
+    METHOD = sys.argv[1]
+    CLIENT_DATA = sys.argv[2]
+    divide = CLIENT_DATA.split("@")[1].split(":")
+    SERVER = divide[0]
+    PORT = int(divide[1])
+except IndexError:
+    sys.exit('Usage: python3 client.py method receiver@IP:SIPport')
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+LINE = ' sip:' + CLIENT_DATA + ' SIP/2.0\r\n'
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
-
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((SERVER, PORT))
 
-    print("Enviando: " + LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    print("Enviando: " + METHOD + LINE)
+    my_socket.send(bytes(METHOD + LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
-
-    print('Recibido -- ', data.decode('utf-8'))
+    response = data.decode('utf-8').split("\r\n")
+    if(response[0].split()[1] == '100' and METHOD == 'INVITE'):
+        my_socket.send(bytes('ACK' + LINE, 'utf-8') + b'\r\n')
+    print('Recibido --', data.decode('utf-8'))
     print("Terminando socket...")
 
 print("Fin.")
--- server.py
+++ server.py
@@ -5,7 +5,14 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
+import os
 
+try:
+    PORT = int(sys.argv[2])
+    AUDIO = sys.argv[3]
+except IndexError:
+    sys.exit("Usage: python3 server.py IP port audio_file")
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
@@ -13,19 +20,32 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
 
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
+        IP = self.client_address[0]
+        PORT = str(self.client_address[1])
+        MP32RTP = './mp32rtp -i 127.0.0.1 -p 23032 <' + AUDIO
+        print("IPCliente: " + IP + " PuertoCliente: " + PORT)
         while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             print("El cliente nos manda " + line.decode('utf-8'))
-
-            # Si no hay más líneas salimos del bucle infinito
-            if not line:
+            if not line or '\n':
                 break
+        method = str.upper(line.decode('utf-8').split(" ")[0])
+        if method == 'INVITE':
+            self.wfile.write(b'SIP/2.0 100 Trying\r\n\r\n')
+            self.wfile.write(b'SIP/2.0 180 Ring\r\n\r\n')
+            self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
+        elif method == 'ACK':
+            os.system(MP32RTP)
+        elif method == 'BYE':
+            self.wfile.write(b'SIP/2.0 200 OK \r\n\r\n')
+        else:
+            self.wfile.write(b'SIP/2.0 405 Method not Allowed\r\n\r\n')
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
-    serv.serve_forever()
+    serv = socketserver.UDPServer(('', PORT), EchoHandler)
+    print("Listening...")
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        sys.exit('Finalizado Servidor')
